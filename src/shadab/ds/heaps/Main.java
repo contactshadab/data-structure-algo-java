c87c41b1a31fc2d0601376c0ecc83e89ee9811d1
Shadab <58193713+contactshadab@users.noreply.github.com>
2018-01-31 21:59:48 +0530

    implemented delete method in Heap

---
+++ src/shadab/ds/heaps/Main.java
@@ -0,0 +1,23 @@
+package shadab.ds.heaps;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MaxHeap heap = new MaxHeap(10);
+		heap.insert(80);
+		heap.insert(75);
+		heap.insert(60);
+		heap.insert(68);
+		heap.insert(55);
+		heap.insert(40);
+		heap.insert(52);
+		heap.insert(67);
+		
+		heap.printHeap();
+		
+		heap.delete(0);
+		heap.printHeap();
+
+	}
+
+}
--- src/shadab/ds/heaps/MaxHeap.java
+++ src/shadab/ds/heaps/MaxHeap.java
@@ -32,8 +32,49 @@ public class MaxHeap {
 		heap[index] = newValue;
 	}
 	
+	private void fixHeapBelow(int index) {
+		int currentValue = heap[index];
+		
+		while(index < size-1 & currentValue < heap[getMaxChild(index)]) {
+			heap[index] = heap[getMaxChild(index)];
+			index = getMaxChild(index);
+		}
+		heap[index] = currentValue;
+	}
+	
+	private int getMaxChild(int index) {
+		//Left child check
+		if((2*index + 1) > size-1)
+			return -1;
+		
+		//Right child check
+		if((2*index + 2) > size-1)
+			return (2*index + 1);
+		
+		return (heap[2*index + 1] > heap[2*index + 2])? (2*index + 1): (2*index + 2);
+	}
+	
 	private int getParent(int index) {
 		return (index-1) / 2;
+	}
+	
+	public int delete(int index) {
+		int deletedValue = heap[index];
+		int replacedValue = heap[size-1];
+		heap[index] = replacedValue;
+		if(index > 0 && heap[index] > heap[getParent(index)]) {
+			fixHeapAbove(index);
+		}else {
+			fixHeapBelow(index);
+		}
+		size--;
+		return deletedValue;
+	}
+	
+	public void printHeap() {
+		for(int i=0; i<size; i++)
+			System.out.print(heap[i] + ", ");
+		System.out.println("");
 	}
 	
 }
