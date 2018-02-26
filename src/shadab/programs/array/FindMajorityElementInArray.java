626ac547c350b796e9c6fdeb38eccb1a5f5f25ff
Shadab <58193713+contactshadab@users.noreply.github.com>
2018-02-26 23:05:27 +0530

    get majority element by moor voting algorithm

--- src/shadab/programs/array/FindMajorityElementInArray.java
+++ src/shadab/programs/array/FindMajorityElementInArray.java
@@ -23,12 +23,42 @@ public class FindMajorityElementInArray {
 		}
 		return Integer.MIN_VALUE;
 	}
+	
+	/*Using Moor Voting algorith
+	Time Complexity: O(n)
+	Space Complexity: O(1)*/
+	public int findByMoorAlgorithm(int[] arr){
+		int majorityElement = arr[0];
+		int count = 1;
+		for(int i=1; i<arr.length; i++){
+			if(arr[i] == majorityElement){
+				count++;
+			}else{
+				count--;
+				if(count == 0){
+					majorityElement = arr[i];
+				}
+			}
+		}
+		count = 0;
+		//Verify if the majority element candidate is actually majority element
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] == majorityElement)
+				count++;
+		}
+		if(count > arr.length/2)
+			return majorityElement;
+		else
+			return Integer.MIN_VALUE;
+	}
 
 	public static void main(String[] args) {
-		int a[] = {2, 2,2,2,2,5,5,2,3,3};
+		int a[] = {2,2,2,5,5,2,3,3,2};
 		
 		FindMajorityElementInArray finder = new FindMajorityElementInArray();
 		System.out.println(finder.findByHashTableMethod(a));
+		
+		System.out.println(finder.findByMoorAlgorithm(a));
 
 	}
 
