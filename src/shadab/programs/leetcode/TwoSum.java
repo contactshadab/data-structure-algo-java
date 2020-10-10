05fc843cf000b79657612277a9fd9a0468566056
Shadab <58193713+contactshadab@users.noreply.github.com>
2020-10-10 20:18:42 +0100

    Sum of two items euual to target, sum of 3 item equal to 0

---
+++ src/shadab/programs/leetcode/TwoSum.java
@@ -0,0 +1,56 @@
+package shadab.programs.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+
+
+
+    Example 1:
+
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+    Example 2:
+
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+    Example 3:
+
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
+
+
+    Constraints:
+
+    2 <= nums.length <= 105
+    -109 <= nums[i] <= 109
+    -109 <= target <= 109
+ */
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target-nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum found equal to be " + target);
+    }
+
+    public static void main(String[] args) {
+        int[] result = new TwoSum().twoSum(new int[]{2,7,11,15}, 9);
+        System.out.println(Arrays.toString(result));
+    }
+}
