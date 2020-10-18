63bea7b0e4b55f96881ab8f3e13fbf7dca0602b4
Shadab <58193713+contactshadab@users.noreply.github.com>
2020-10-18 12:07:45 +0100

    avl tree

---
+++ src/shadab/programs/avlTree/MyAVLTree.java
@@ -0,0 +1,63 @@
+package shadab.programs.avlTree;
+
+import javax.sound.midi.Soundbank;
+
+public class MyAVLTree {
+    private class Node {
+        int value;
+        Node leftChild;
+        Node rightChild;
+        int height;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return Integer.toString(value);
+        }
+    }
+
+    Node root;
+
+    private int height(Node root) {
+        return (root == null) ? -1 : root.height;
+    }
+
+    private Node insert(Node root, int value) {
+        if (root == null) {
+            return new Node(value);
+        }
+
+        if (value < root.value) {
+            root.leftChild = insert(root.leftChild, value);
+        } else {
+            root.rightChild = insert(root.rightChild, value);
+        }
+
+        root.height = 1 + Math.max(height(root.leftChild), height(root.rightChild));
+
+        int balanceFactor = height(root.leftChild) - height(root.rightChild);
+        if (balanceFactor <= 1) {
+            System.out.println("Balanced");
+        } else {
+            System.out.println("Not Balanced");
+        }
+
+        return root;
+    }
+
+    public void insert(int value) {
+        root = insert(root, value);
+    }
+
+    public static void main(String[] args) {
+        MyAVLTree avlTree = new MyAVLTree();
+        avlTree.insert(10);
+        avlTree.insert(20);
+        avlTree.insert(30);
+        avlTree.insert(1);
+        avlTree.insert(25);
+    }
+}
